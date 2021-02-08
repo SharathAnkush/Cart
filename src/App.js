ff9b061aeeced691ed046f21a78e198ad45e1ad8
sharathankush <sharathankush@gmail.com>
2021-02-08 06:29:48 +0530

    added cart methods

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import React,{useState} from 'react'
 
 function App() {
+  const [ cart, setCart ] = useState([])
+ 
+ //methods
+ //checking the buyitem is already in cart or not
+
+const addInCart = (item) => {
+  const alreadyInCart = cart.findIndex(array => array.id === item.id)
+  
+  if(alreadyInCart !== -1){
+     alert('already in cart')
+  }
+
+  setCart([...cart,item])
+
+}
+//Buy method
+const Buy = () => {
+ setCart([]);
+ alert('added in cart')
+}
+
+//remove from cart
+const Remove = () =>{
+ setCart(cart.findIndex(single => single.id !== item.id))
+}
+
+
   return (
     <div className="App">
       <h1>cart</h1>
